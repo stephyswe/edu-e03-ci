c9e701dd2b4d73805be6d879edc9282eb347486c
Stephanie <stephaniiehallberg@gmail.com>
2023-04-26 18:22:04 +0200

    feat(calc): move to h

--- calculator.c
+++ calculator.c
@@ -1,27 +1,8 @@
+#include "calculator.h"
+
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-
-#include "input.h"
-
-typedef enum {
-    Calculator_Status_Ok = 0,
-    Calculator_Status_InvalidInput = 1,
-    Calculator_Status_DivideByZero = 2
-} Calculator_Status;
-
-#define OPERATOR_ADD '+'
-#define OPERATOR_SUB '-'
-#define OPERATOR_MUL '*'
-#define OPERATOR_DIV '/'
-#define OPERATOR_MOD '%'
-
-#define GET_INPUT(prompt, var)                                               \
-    while ((status = getInputValue(prompt, &var)) != Calculator_Status_Ok) { \
-        printf("Invalid input for " #var ". Please try again.\n");           \
-    }
-
-typedef double (*OperatorFunction)(double, double);
 
 double addition(double num1, double num2) { return num1 + num2; }
 
@@ -33,38 +14,21 @@ double divide(double num1, double num2) { return num1 / num2; }
 
 double modulus(double num1, double num2) { return fmod(num1, num2); }
 
-// what does fmod do?
-// is fmod same as modulus operator? Answer is yes
-
 char getValidOperator() {
     const char* VALID_OPERATORS = "+-*/%";
     char op;
 
-    while (true) {
+    do {
         printf("Enter operator (%s): ", VALID_OPERATORS);
-        scanf(" %c", &op);
-
-        // Check if the first character of the input is a valid operator
-        if (op != '\n' && strchr(VALID_OPERATORS, op) != NULL) {
-            // Clear the input buffer
-            scanf("%*[^\n]");
-            scanf("%*c");
-            return op;
-        } else {
+        scanf(" %c%*[^\n]", &op);
+        scanf("%*c");
+        if (strchr(VALID_OPERATORS, op) == NULL) {
             printf("Invalid operator. Please try again.\n");
-            // clear the input buffer
-            scanf("%*[^\n]");
-            scanf("%*c");
         }
-    }
+    } while (strchr(VALID_OPERATORS, op) == NULL);
+
+    return op;
 }
-
-OperatorFunction opFunction = NULL;
-
-typedef struct {
-    char operatorChar;
-    OperatorFunction operatorFunction;
-} OperatorMapping;
 
 OperatorMapping operatorMappings[] = {{OPERATOR_ADD, addition},
                                       {OPERATOR_SUB, subtract},
@@ -83,14 +47,13 @@ OperatorFunction getOperatorFunction(char op) {
 }
 
 void calculatorMenu() {
-    printf("Calculator menu\n");
-
     double num1, num2, result;
     char op;
-    Calculator_Status status = Calculator_Status_Ok;
 
-    GET_INPUT("num1: ", num1);
-    GET_INPUT("num2: ", num2);
+    printf("Calculator menu\n");
+
+    getInputValue("num1: ", &num1);
+    getInputValue("num2: ", &num2);
 
     op = getValidOperator();
     // Perform the operation
--- calculator.h
+++ calculator.h
@@ -1,5 +1,26 @@
+#include "input.h"
+
 #ifndef __CALCULATOR_H_
 #define __CALCULATOR_H_
+
+typedef double (*OperatorFunction)(double, double);
+
+typedef struct {
+    char operatorChar;
+    OperatorFunction operatorFunction;
+} OperatorMapping;
+
+typedef enum {
+    Calculator_Status_Ok = 0,
+    Calculator_Status_InvalidInput = 1,
+    Calculator_Status_DivideByZero = 2
+} Calculator_Status;
+
+#define OPERATOR_ADD '+'
+#define OPERATOR_SUB '-'
+#define OPERATOR_MUL '*'
+#define OPERATOR_DIV '/'
+#define OPERATOR_MOD '%'
 
 void calculatorMenu();
 
