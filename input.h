02f07c6764b22de3e9fddfe77b73c388387646fc
Stephanie <stephaniiehallberg@gmail.com>
2023-04-27 12:01:12 +0200

    feat(input): remove input-list

--- input.h
+++ input.h
@@ -14,12 +14,6 @@ typedef enum {
     INPUT_RESULT_INVALID_INPUT
 } INPUT_RESULT;
 
-typedef struct {
-    const char **validList;
-    int numValidList;
-    const char *errorMsg;
-} InputList;
-
 INPUT_RESULT GetInput(char *prompt, char *buff, int maxSize);
 void getInputValue(char *param, double *value);
 void getValidInput(const char *prompt, const char *validList[],
